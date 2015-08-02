a1f3098d458bc720519877cb5b2a47c5a8c0c90f
Cole Smith <lcs1956@me.com>
2015-08-02 14:26:07 -0400

    Update geometry.pl
    Repaired previous commit by moving G4TRAP parameters up to lines 387-397.

--- clas12/ec/geometry.pl
+++ clas12/ec/geometry.pl
@@ -382,7 +382,20 @@ sub build_scintlayers
 	
 	$description ="Forward Calorimeter scintillator layer ${i}";
     $pos = "$xscint*mm $yscint*mm $zscint*mm";
+# a scintillator layer first. set the G4Trap parameters.
 
+    my $pDx1   = 0.001;  # should be zero, but that makes gemc crash.
+    my $pDx2   = &spDx2(1);
+    my $pDz    = $dscint/2.0;  #<-------------------------------------
+    my $pTheta = 0;
+    my $pPhi   = $pTheta;
+    my $pDy1   = &spDy(1);
+    my $pDy2   = $pDy1;
+    my $pDx3   = $pDx1;
+    my $pDx4   = $pDx2;
+    my $pAlp1  = $pTheta;
+    my $pAlp2  = $pTheta;
+    
     my %detector = init_det();
     $detector{"name"}        = $subname;
     $detector{"mother"}      = "ec_s".$sector;
@@ -400,19 +413,7 @@ sub build_scintlayers
     print_det(\%configuration, \%detector);
 
 	# loop over layers and generates Geant4 parameters for each scint and lead layer.
-# a scintillator layer first. set the G4Trap parameters.
-#
-    my $pDx1   = 0.001;  # should be zero, but that makes gemc crash.
-    my $pDx2   = &spDx2(1);
-    my $pDz    = $dscint/2.0;  #<-------------------------------------
-    my $pTheta = 0;
-    my $pPhi   = $pTheta;
-    my $pDy1   = &spDy(1);
-    my $pDy2   = $pDy1;
-    my $pDx3   = $pDx1;
-    my $pDx4   = $pDx2;
-    my $pAlp1  = $pTheta;
-    my $pAlp2  = $pTheta;
+
 
     for ($i = 2; $i <= $nlayers; $i++)
 	{

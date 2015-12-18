47acc8f176efc27d807f5191f621395292af1c87
dmriser <dmriser@gmail.com>
2015-12-18 14:34:37 -0500

    Prepared downstream.pl and beamline.pl for merge..

--- clas12/beamline/beamline.pl
+++ clas12/beamline/beamline.pl
@@ -1,6 +1,5 @@
 #!/usr/bin/perl -w
 
-
 use strict;
 use lib ("$ENV{GEMC}/api/perl");
 use utils;
@@ -23,7 +22,7 @@ sub help()
 }
 
 # Make sure the argument list is correct
-if( scalar @ARGV != 1) 
+if( scalar @ARGV != 1)
 {
 	help();
 	exit;
@@ -31,15 +30,14 @@ if( scalar @ARGV != 1)
 
 # Loading configuration file and paramters
 our %configuration = load_configuration($ARGV[0]);
-#my %parameters    = get_parameters(%configuration); 
-
+#my %parameters    = get_parameters(%configuration);
 
 
 # Global parameters
 # Downstream beamline is a 4cm thick pipe of lead, with OD = 350 mm
 
 # General:
-our $inches   = 25.4;
+our $inches    = 25.4;
 our $degrad    = 57.27;
 
 # Torus numbers:
@@ -47,19 +45,29 @@ our $TorusZpos        = 151.855*$inches;                 # center of the torus p
 our $SteelFrameLength = 94.*$inches/2.0;                 # 1/2 length
 
 
-require "./noft_moller_shield.pl";
 require "./downstream.pl";
+require "./noft_moller_shield.pl";
 require "./ft_moller_shield.pl";
 require "./tagger.pl";
 
+# these are the values present in the downstream.pl
+# BEFORE I changed anything at all.
 $configuration{"variation"} = "noft" ;
-#make_moller_shield();
-#make_downstream_shielding();
-make_tagger();
+make_moller_shield();
+make_downstream_shielding(300.0,170.0,"beamline_W");
+make_beamline_torus();
 
 $configuration{"variation"} = "ft" ;
-#make_moller_shield_ft();
-#make_downstream_shielding();
+#make_tagger();
 
+# 10" depth, extra thick, considered the nominal working design
+$configuration{"variation"} = "noft-l254-r195.4";
+make_moller_shield();
+make_beamline_torus();
+make_downstream_shielding(254.0,195.4,"beamline_W");
 
-
+# without nose
+$configuration{"variation"} = "baseline";
+make_moller_shield();
+make_beamline_torus();
+make_orig();

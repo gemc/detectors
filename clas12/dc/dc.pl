c195fca623a1fc581c4146a3ad3077bbfd1e449e
maureeungaro <maurizio.ungaro@cnu.edu>
2016-03-01 15:35:17 -0500

    only using ccdb version from now on

--- clas12/dc/dc.pl
+++ clas12/dc/dc.pl
@@ -59,7 +59,7 @@ require "./utils.pl";
 
 # all the scripts must be run for every configuration
 # Right now run both configurations, later on just ccdb
-my @allConfs = ("original", "ccdb");
+my @allConfs = ("ccdb");
 
 foreach my $conf ( @allConfs )
 {

81c505829420188a0aeca522f5f2a12665a87b09
maureeungaro <maurizio.ungaro@cnu.edu>
2016-03-11 16:38:52 -0500

    ddvcs template for pbw4 crystals

--- clas12/ddvcs/ddvcs.pl
+++ clas12/ddvcs/ddvcs.pl
@@ -6,6 +6,7 @@ use utils;
 use parameters;
 use geometry;
 use math;
+use materials;
 
 use Math::Trig;
 
@@ -14,7 +15,7 @@ sub help()
 {
 	print "\n Usage: \n";
 	print "   geometry.pl <configuration filename>\n";
- 	print "   Will create the CLAS12 High Threshold Cherenkov Counter (htcc) using the variation specified in the configuration file\n";
+ 	print "   Will create the DDVS setup using the variation specified in the configuration file\n";
  	print "   Note: The passport and .visa files must be present to connect to MYSQL. \n\n";
 	exit;
 }
@@ -30,15 +31,34 @@ if( scalar @ARGV != 1)
 # Loading configuration file from argument
 our %configuration = load_configuration($ARGV[0]);
 
-# One can change the "variation" here if one is desired different from the config.dat
-# $configuration{"variation"} = "myvar";
 
-# Load the parameters
-# our %parameters    = get_parameters(%configuration);
+# materials
+require "./materials.pl";
 
 
 # Loading DDVCS geometry routines specific subroutines
-require "./ddvcsCone.pl";
+#require "./ddvcsCone.pl";
+require "./muCal.pl";
 
 
-build_ddvcsCone();
+#build_ddvcsCone();
+
+
+make_mu_cal();
+
+# all the scripts must be run for every configuration
+my @allConfs = ("original");
+
+foreach my $conf ( @allConfs )
+{
+	$configuration{"variation"} = $conf ;
+	
+	# materials
+	materials();
+	
+	# geometry
+	make_mu_cal();
+	
+}
+
+

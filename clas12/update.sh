79e6fb27011d853ddc76b8cfe89276a1cad29abf
Andrey Kim <drewkenjo@users.noreply.github.com>
2016-04-04 19:03:39 -0400

    revert update.sh to Gagik's version

--- clas12/update.sh
+++ clas12/update.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 rm coat*jar
-#wget http://clasweb.jlab.org/clas12maven/org/jlab/coat/coat-libs/2.0-SNAPSHOT/coat-libs-2.0-SNAPSHOT.jar
-wget http://userweb.jlab.org/~kenjo/coat-libs-2.0-SNAPSHOT.jar
+wget http://clasweb.jlab.org/clas12maven/org/jlab/coat/coat-libs/2.0-SNAPSHOT/coat-libs-2.0-SNAPSHOT.jar
+#wget http://userweb.jlab.org/~kenjo/coat-libs-2.0-SNAPSHOT.jar
 

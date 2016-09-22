08d1200b7772e57bc0fa2570f5ebe4386596ee12
Andrew Kim <drewkenjo@gmail.com>
2016-09-22 08:49:43 -0400

    added vecmath jar

--- clas12/update.sh
+++ clas12/update.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
-rm coat*jar jcsg*jar
+rm coat*jar jcsg*jar vecmath*jar
 wget http://clasweb.jlab.org/clas12maven/org/jlab/coat/coat-libs/2.4-SNAPSHOT/coat-libs-2.4-SNAPSHOT.jar
 #wget http://userweb.jlab.org/~kenjo/coat-libs-2.4-SNAPSHOT.jar
 wget http://userweb.jlab.org/~kenjo/geom/jcsg-0.3.2.jar
+wget http://userweb.jlab.org/~kenjo/geom/vecmath-1.3.1.jar
 

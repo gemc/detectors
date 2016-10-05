4903218e363defe2065fea238531f8f643ddefbe
Maurizio Ungaro <maurizio.ungaro@cnu.edu>
2016-10-05 10:27:05 -0400

    added tdc variable for clas12 dc

--- clas12/dc/bank.pl
+++ clas12/dc/bank.pl
@@ -26,11 +26,12 @@ sub define_bank
 	insert_bank_variable(\%configuration, $bankname, "superlayer",  2,  "Di", "superlayer index");
 	insert_bank_variable(\%configuration, $bankname, "layer",       3,  "Di", "layer index");
 	insert_bank_variable(\%configuration, $bankname, "wire",        4,  "Di", "wire index");
-	insert_bank_variable(\%configuration, $bankname, "LR",          5,  "Di", "Left/Right: -1 (right) if the track is between the beam and the closest wire");
-	insert_bank_variable(\%configuration, $bankname, "doca",        6,  "Dd", "2D distance between closest track step to the wire hit");
-	insert_bank_variable(\%configuration, $bankname, "sdoca",       7,  "Dd", "smeared doca");
-	insert_bank_variable(\%configuration, $bankname, "time",        8,  "Dd", "doca / drift velocity in each region");
-	insert_bank_variable(\%configuration, $bankname, "stime",       9,  "Dd", "sdoca / drift velocity in each region");
+	insert_bank_variable(\%configuration, $bankname, "tdc",         5,  "Di", "tdc value");
+	insert_bank_variable(\%configuration, $bankname, "LR",          6,  "Di", "Left/Right: -1 (right) if the track is between the beam and the closest wire");
+	insert_bank_variable(\%configuration, $bankname, "doca",        7,  "Dd", "2D distance between closest track step to the wire hit");
+	insert_bank_variable(\%configuration, $bankname, "sdoca",       8,  "Dd", "smeared doca");
+	insert_bank_variable(\%configuration, $bankname, "time",        9,  "Dd", "doca / drift velocity in each region");
+	insert_bank_variable(\%configuration, $bankname, "stime",      10,  "Dd", "sdoca / drift velocity in each region");
 	insert_bank_variable(\%configuration, $bankname, "hitn",       99,  "Di", "hit number");
 }
 

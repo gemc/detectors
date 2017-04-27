1d8447975c8ebef17b1d0f68bac8ed3e7863489a
Maurizio Ungaro <maurizio.ungaro@cnu.edu>
2017-04-27 15:29:47 -0400

    improved syntax

--- basic_examples/1_Simple_detector/ex1_1_simple_det/geometry.py
+++ basic_examples/1_Simple_detector/ex1_1_simple_det/geometry.py
@@ -4,18 +4,15 @@ def makeGeometry(configuration):
 	# volume fields can be given either as named arguments in the MyDetector() call or  assigned later to the instance variable
 	detector = MyDetector(name="my_det", mother="root")	
 	detector.description = "Si detector"
-	detector.pos = "0.*cm 0.*cm 0.*cm"
-	detector.rotation = "0*deg 0*deg 0*deg"
-	detector.color = "f4a988"
-	detector.type = "Tube"
+	detector.color       = "f4a988"
 	# Tube shape dimensions are:  inner_radius, outer_radius, half-length, starting_angle, total angle
 	# A non-zero inner radius will produce a hollow tube.  The angles allow for an angular cut in the cross section
+	detector.type        = "Tube"
 	detector.dimensions = "0.*cm 1.*cm 5.*mm 0*deg 360*deg"
-	detector.material = "G4_Si"	# G4_Si is a GEANT4 defined element name
+	detector.material   = "G4_Si"	# G4_Si is a GEANT4 defined element name
 							# When using only built-in materials, a separate materials file is not needed
-	detector.mfield = "no"
 	detector.visible = 1			# 1 to display volume with the full geometry, 0 to leave hidden
-	detector.style = 1			# 1 displays volume as a solid, 0 displays as wireframe
+	detector.style = 1			    # 1 displays volume as a solid, 0 displays as wireframe
 	detector.sensitivity = "flux"	# Every track through the volume will generate a hit
 	detector.hit_type = "flux"		# Every track through the volume will generate a hit
 	detector.identifiers = "1"		# Identifies the detector being hit:  for FLUX detector this is an integer value

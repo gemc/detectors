57a0cb4a54adc2e60f708bb795ebb3efe4ad10af
RichmondPhysics <ggilfoyl@richmond.edu>
2017-05-10 14:39:07 -0400

    Update to coatjava-4a.0.0
    Bringing SVT geometry up-to-date for publishing CLAS-NOTE. - gpg

--- clas12/bst/factory.groovy
+++ clas12/bst/factory.groovy
@@ -1,11 +1,16 @@
 
 //============================================================
-// This script prints volumes for SVT detector
+// This script prints volumes for SVT detector into the file
+// bst__volumes_java.txt. Used in bst.pl.
+//
+// Using coatjava-4a.0.0 - 17-may-08 gpg
 //============================================================
 import org.jlab.geom.base.*;
 import org.jlab.clasrec.utils.*;
 import org.jlab.detector.geant4.v2.*;
+// JCSG
 import org.jlab.detector.geant4.v2.SVT.*;
+import org.jlab.detector.volume.*;
 import org.jlab.detector.calib.utils.DatabaseConstantProvider;
 
 //println "classpath dump:"
@@ -13,14 +18,14 @@ import org.jlab.detector.calib.utils.DatabaseConstantProvider;
 //System.exit(0);
 
 SVTConstants.VERBOSE = true;
-ConstantProvider cp = new DatabaseConstantProvider(10, "default");
+DatabaseConstantProvider cp = new DatabaseConstantProvider(11, "default");
 SVTConstants.connect(cp);
 
 SVTVolumeFactory factory = new SVTVolumeFactory( cp, false ); // ideal geometry
 //SVTConstants.loadAlignmentShifts("shifts_test.dat"); // load alignment shifts from file
 //factory.setApplyAlignmentShifts( true ); // shifted geometry
 
-//factory.VERBOSE = true;
+factory.VERBOSE = true;
 factory.BUILDSENSORS = true; // include physical sensors (aka cards)
 factory.HALFBOXES = true; // geant4 wants half dimensions for boxes (but not tube radii)
 factory.makeVolumes();

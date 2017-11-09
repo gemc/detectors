a1691870fdf4946b2e2068aa016f05ff63fcc287
Maurizio Ungaro <maurizio.ungaro@cnu.edu>
2017-11-09 16:22:04 -0500

    playing with vars

--- clas12/ctof/plugin/hitDigitization.cc
+++ clas12/ctof/plugin/hitDigitization.cc
@@ -14,9 +14,26 @@ GObservables* ctofPlugin::digitizeHit(GHit *ghit)
 	
 	gdata->addObservable(ivar,                    // variable value
 						 "ivar",                  // variable name
-						 "digitized double var",  // variable description
+						 "digitized int var",  // variable description
 						 "int");                  // save to disk as type
 
+	ivar = 44;
+	gdata->addObservable(ivar,                    // variable value
+						 "ivar2",                  // variable name
+						 "digitized int var2",  // variable description
+						 "int");                  // save to disk as type
+	ivar = 55;
+	gdata->addObservable(ivar,                    // variable value
+						 "ivar3",                  // variable name
+						 "digitized int var3",  // variable description
+						 "int");                  // save to disk as type
+
+	float exampleF = 3.2;
+	gdata->addObservable(exampleF,                    // variable value
+						 "exampleF",                  // variable name
+						 "digitized float var",  // variable description
+						 "float");                  // save to disk as type
+	
 
 	return gdata;
 }

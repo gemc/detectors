b239344001ec9768a5f5d98b2709efa8483404ee
Maurizio Ungaro <maurizio.ungaro@cnu.edu>
2017-11-16 15:50:43 -0500

    units instead of var description

--- clas12/ctof/plugin/hitDigitization.cc
+++ clas12/ctof/plugin/hitDigitization.cc
@@ -7,33 +7,32 @@ GObservables* ctofPlugin::digitizeHit(GHit *ghit)
 	double dvar = 2.22;
 	int ivar = 33;
 
-	gdata->addObservable(dvar,                    // variable value
-						 "dvar",                  // variable name
-						 "digitized double var",  // variable description
-						 "double");               // save to disk as type
+	gdata->addObservable(dvar,       // variable value
+						 "dvar",     // variable name
+						 "m",        // variable units
+						 "double");  // save to disk as type
 	
-	gdata->addObservable(ivar,                    // variable value
-						 "ivar",                  // variable name
-						 "digitized int var",  // variable description
-						 "int");                  // save to disk as type
+	gdata->addObservable(ivar,       // variable value
+						 "ivar",     // variable name
+						 "count",    // variable units
+						 "int");     // save to disk as type
 
 	ivar = 44;
-	gdata->addObservable(ivar,                    // variable value
-						 "ivar2",                  // variable name
-						 "digitized int var2",  // variable description
-						 "int");                  // save to disk as type
+	gdata->addObservable(ivar,       // variable value
+						 "ivar2",    // variable name
+						 "ns",       // variable units
+						 "int");     // save to disk as type
 	ivar = 55;
-	gdata->addObservable(ivar,                    // variable value
-						 "ivar3",                  // variable name
-						 "digitized int var3",  // variable description
-						 "int");                  // save to disk as type
+	gdata->addObservable(ivar,       // variable value
+						 "ivar3",    // variable name
+						 "ms",       // variable units
+						 "int");     // save to disk as type
 
 	float exampleF = 3.2;
-	gdata->addObservable(exampleF,                    // variable value
-						 "exampleF",                  // variable name
-						 "digitized float var",  // variable description
-						 "float");                  // save to disk as type
+	gdata->addObservable(exampleF,   // variable value
+						 "exampleF", // variable name
+						 "cm",       // variable units
+						 "float");   // save to disk as type
 	
-
 	return gdata;
 }

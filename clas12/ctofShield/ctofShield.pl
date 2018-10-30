756ee2616efb02c9db43762eb188a4aad9fd6adf
Maurizio Ungaro <maurizio.ungaro@cnu.edu>
2018-10-30 14:39:37 -0400

    added ctof shield configurations, 1,2,3 mm

---
+++ clas12/ctofShield/ctofShield.pl
@@ -0,0 +1,70 @@
+#!/usr/bin/perl -w
+
+
+use strict;
+use lib ("$ENV{GEMC}/api/perl");
+use utils;
+use geometry;
+
+use Math::Trig;
+
+# Help Message
+sub help()
+{
+	print "\n Usage: \n";
+	print "   ctofShield.pl <configuration filename>\n";
+ 	print "   Will create the CLAS12 bst shield in various configurations\n";
+	exit;
+}
+
+# Make sure the argument list is correct
+if( scalar @ARGV != 1) 
+{
+	help();
+	exit;
+}
+
+# Loading configuration file and paramters
+our %configuration = load_configuration($ARGV[0]);
+
+my @allConfs = ("w1", "w2", "w3");
+
+my $rmin   = 240;
+my $length = 420;
+my $pos    = "0*mm 0*mm -80*mm";
+
+foreach my $conf ( @allConfs )
+{
+	$configuration{"variation"} = $conf ;
+	
+	my %detector = init_det();
+	
+	$detector{"name"}        = "ctofShield";
+	$detector{"mother"}      = "root";
+	$detector{"description"} = "ctof tungsten shielding";
+	$detector{"color"}       = "ff88aa";
+	$detector{"type"}        = "Tube";
+	$detector{"pos"}         =  $pos ;
+	$detector{"material"}    = "G4_W";
+
+	my $rmax = 0;
+
+	if($conf eq "w1") {
+		$rmax = $rmin + 1;
+	} elsif($conf eq "w2") {
+		$rmax = $rmin + 2;
+	} elsif($conf eq "w3") {
+		$rmax = $rmin + 3;
+	}
+	
+	my $dimen = "$rmin*mm $rmax*mm $length*mm 0*deg 360*deg";
+
+	$detector{"dimensions"}  = $dimen;
+	$detector{"visible"}     = 1;
+	$detector{"style"}       = 1;
+	print_det(\%configuration, \%detector);
+}
+
+
+
+

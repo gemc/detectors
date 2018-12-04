1d006574751ceec7abfcf32ff4aa3510b045f3fd
Maurizio Ungaro <maurizio.ungaro@cnu.edu>
2018-12-04 14:57:33 -0500

    update script using custom wget

--- clas12/update.sh
+++ clas12/update.sh
@@ -1,11 +1,19 @@
 #!/bin/tcsh -f
 
+# Linux: -c = do not get file if already done.
+if (`uname` == "Linux") then
+	set mwget = "wget -c -nv --no-check-certificate"
+# Mac: makes it quite and show nice progress
+else if (`uname` == "Darwin") then
+	set mwget = "wget -qc --show-progress --no-check-certificate"
+endif
+
+
 rm -rf coat*jar jcsg*jar vecmath*jar
 
 set COATJAVA = 5b.7.1
 
-#temporary for DC cell shift verion: tag 5c.6.9
-wget --show-progress --no-check-certificate --trust-server-names https://github.com/JeffersonLab/clas12-offline-software/releases/download/$COATJAVA/coatjava-$COATJAVA".tar.gz" -O coatjava-$COATJAVA".tar.gz"
+$mwget  --trust-server-names https://github.com/JeffersonLab/clas12-offline-software/releases/download/$COATJAVA/coatjava-$COATJAVA".tar.gz" -O coatjava-$COATJAVA".tar.gz"
 tar -zxvf coatjava-$COATJAVA".tar.gz"
 cp coatjava/lib/clas/* .
 

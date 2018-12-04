26fce272d2e3755bbd9e0c66398afa9193bacc50
Maurizio Ungaro <maurizio.ungaro@cnu.edu>
2018-12-04 14:55:31 -0500

    removing version from copying from coatjava/lib

--- clas12/update.sh
+++ clas12/update.sh
@@ -7,5 +7,5 @@ set COATJAVA = 5b.7.1
 #temporary for DC cell shift verion: tag 5c.6.9
 wget --show-progress --no-check-certificate --trust-server-names https://github.com/JeffersonLab/clas12-offline-software/releases/download/$COATJAVA/coatjava-$COATJAVA".tar.gz" -O coatjava-$COATJAVA".tar.gz"
 tar -zxvf coatjava-$COATJAVA".tar.gz"
-cp coatjava/lib/clas/coat-libs-5.1-SNAPSHOT.jar .
+cp coatjava/lib/clas/* .
 

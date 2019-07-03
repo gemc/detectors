87df86f339047dd901f015248133b32ef1b9e1fa
Maurizio Ungaro <maurizio.ungaro@cnu.edu>
2019-07-03 10:59:50 -0400

    added devel case for coatjava

--- clas12/update.sh
+++ clas12/update.sh
@@ -12,10 +12,21 @@ endif
 rm -rf coat*jar jcsg*jar vecmath*jar
 
 set COATJAVA = 6b.2.0
+#set COATJAVA = devel
 
-echo Dowloading coatjava version $COATJAVA from: https://github.com/JeffersonLab/clas12-offline-software/releases/download/$COATJAVA/coatjava-$COATJAVA".tar.gz"
+if ($COATJAVA != "devel") then
 
-$mwget  --trust-server-names https://github.com/JeffersonLab/clas12-offline-software/releases/download/$COATJAVA/coatjava-$COATJAVA".tar.gz" -O coatjava-$COATJAVA".tar.gz"
-tar -xvf coatjava-$COATJAVA".tar.gz"
-cp coatjava/lib/clas/* .
+	echo Dowloading coatjava version $COATJAVA from: https://github.com/JeffersonLab/clas12-offline-software/releases/download/$COATJAVA/coatjava-$COATJAVA".tar.gz"
+
+	$mwget  --trust-server-names https://github.com/JeffersonLab/clas12-offline-software/releases/download/$COATJAVA/coatjava-$COATJAVA".tar.gz" -O coatjava-$COATJAVA".tar.gz"
+	tar -xvf coatjava-$COATJAVA".tar.gz"
+	cp coatjava/lib/clas/* .
+else
+	echo Cloning https://github.com/jeffersonlab/clas12-offline-software and building coatjava
+	# assuming maven is installed
+	git clone https://github.com/jeffersonlab/clas12-offline-software
+	cd clas12-offline-software
+	./build-coatjava.sh
+	cp coatjava/lib/clas/* ..
+endif
 

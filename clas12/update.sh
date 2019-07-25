e77dd1be233f85f6ea82f86f3e5f749c7670b355
Maurizio Ungaro <maurizio.ungaro@cnu.edu>
2019-07-25 14:08:00 -0400

    updated gcard to use GEMC_DATA_DIR where experiments is located

--- clas12/update.sh
+++ clas12/update.sh
@@ -12,21 +12,14 @@ endif
 rm -rf coat*jar jcsg*jar vecmath*jar
 
 
-set USEDEVEL = yes
+set USEDEVEL = "no"
 
 set COATJAVA = 6b.3.0
 set BRANCH   = gemcPrecision
+rm -rf clas12-offline-software
 
-if ($USEDEVEL != "yes") then
-
-	echo Dowloading coatjava version $COATJAVA from: https://github.com/JeffersonLab/clas12-offline-software/releases/download/$COATJAVA/coatjava-$COATJAVA".tar.gz"
-
-	$mwget  --trust-server-names https://github.com/JeffersonLab/clas12-offline-software/releases/download/$COATJAVA/coatjava-$COATJAVA".tar.gz" -O coatjava-$COATJAVA".tar.gz"
-	tar -xvf coatjava-$COATJAVA".tar.gz"
-	cp coatjava/lib/clas/* .
-
-else
-	rm -rf clas12-offline-software
+if ($USEDEVEL == "yes") then
+	# DEVELOPMENT, CLONING REPO AND USING BRANCH
 	echo Cloning https://github.com/jeffersonlab/clas12-offline-software and building coatjava
 
 	# assuming maven is installed
@@ -39,5 +32,12 @@ else
 	./build-coatjava.sh
 	cp coatjava/lib/clas/* ..
 
+else
+	# NOT DEVELOPMENT, DOWNOADING COATJAVA TAGGED RELEASE
+	echo Dowloading coatjava version $COATJAVA from: https://github.com/JeffersonLab/clas12-offline-software/releases/download/$COATJAVA/coatjava-$COATJAVA".tar.gz"
+
+	$mwget  --trust-server-names https://github.com/JeffersonLab/clas12-offline-software/releases/download/$COATJAVA/coatjava-$COATJAVA".tar.gz" -O coatjava-$COATJAVA".tar.gz"
+	tar -xvf coatjava-$COATJAVA".tar.gz"
+	cp coatjava/lib/clas/* .
 endif
 

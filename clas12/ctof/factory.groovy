9f992f77e6d17104fc9799ddf0b364a952742a80
Andrew Kim <drewkenjo@gmail.com>
2019-09-24 11:57:46 -0400

    export upstream lightguides into separate folder

--- clas12/ctof/factory.groovy
+++ clas12/ctof/factory.groovy
@@ -29,6 +29,35 @@ factory.getComponents().forEach{ volume ->
 }
 
 new File(dirName+'_upstream').mkdir();
-factory.getAllVolumes().findAll{it.getType()=='Stl' && it.getName().startsWith('lgd')}.each{
-  it.getPrimitive().toCSG().toStlFile(sprintf("%s_upstream/%s.stl", [dirName, it.getName()]))
+
+import eu.mihosoft.vrl.v3d.Vector3d;
+import org.jlab.detector.units.SystemOfUnits.Length;
+import org.jlab.detector.geant4.v2.CTOFGeant4Factory.CTOFpaddle
+import java.io.InputStream
+
+String caddbpath  = "/geometry/ctof/cad/"
+double globalOffset = cp.getDouble("/geometry/target/position", 0)
+double cadRadius  = cp.getDouble(caddbpath+"radius", 0)
+double cadThick   = cp.getDouble(caddbpath+"thickness", 0)
+double cadAngle   = cp.getDouble(caddbpath+"angle", 0)
+double cadOffset  = cp.getDouble(caddbpath+"offset", 0)
+
+(0..100).collect{
+   InputStream strm = CTOFGeant4Factory.class.getClassLoader().getResourceAsStream(sprintf('ctof/cad/lgu%02d.stl',it))
+   if(strm!=null) {
+     def component = new CTOFpaddle(factory, cp, String.format("lgu%02d", it), strm, it)
+     component.scale(Length.mm / Length.cm)
+
+     component.rotate("zyx", Math.toRadians(90+cadAngle), Math.toRadians(180), 0)
+
+     Vector3d idealCenter = new Vector3d(cadRadius+cadThick/2.0,0, 0)
+     idealCenter.rotateZ(Math.toRadians(cadAngle*(it-0.5)));
+
+     Vector3d currentCenter = component.center
+     Vector3d shift = currentCenter.clone().sub(idealCenter)
+     component.translate(shift.x, shift.y, shift.z+cadOffset+globalOffset)
+     component.getPrimitive().toCSG().toStlFile(sprintf("%s_upstream/%s.stl", [dirName, component.getName()]))
+   }
 }
+
+

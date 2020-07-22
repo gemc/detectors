b83fd925817bc8adfd0f521fc556598c68d1832d
maureeungaro <maureeungaro@gmail.com>
2020-07-22 09:25:29 -0400

    added flux detector and finalized dimensions

--- clas12/targets/geometry.pl
+++ clas12/targets/geometry.pl
@@ -10,36 +10,91 @@ sub build_targets
 
 	if($thisVariation eq "pbTest" )
 	{
-		# PB 125 microns
-		my $Rout       = 5;  #
-		my $length     = 0.0625; # (0.125 mm thick)
+		# mother is a helium bag 293.26 mm long. Its center is 40.85 mm
+		my $tshift     = 40.85;
+		my $Rout       = 10;  #
+		my $Celllength     = 293.26 / 2;
+		my $zpos       = $Celllength - $tshift;
 		my %detector = init_det();
-		$detector{"name"}        = "testPbTarget";
+		$detector{"name"}        = "targetCell";
 		$detector{"mother"}      = "root";
-		$detector{"description"} = "Pb Foil for Nuclear Targets Test";
-		$detector{"color"}       = "aa0000";
+		$detector{"description"} = "Helium cell";
+		$detector{"color"}       = "5511111";
 		$detector{"type"}        = "Tube";
+		$detector{"pos"}         = "0 0 $zpos*mm";
+		$detector{"dimensions"}  = "0*mm $Rout*mm $Celllength*mm 0*deg 360*deg";
+		$detector{"material"}    = "G4_He";
+		$detector{"style"}       = "1";
+		print_det(\%configuration, \%detector);
+
+		# PB 125 microns
+		$Rout       = 5;  #
+		$zpos       = $tshift - $Celllength;
+		my $length     = 0.0625; # (0.125 mm thick)
+		%detector = init_det();
+		$detector{"name"}        = "testPbTarget";
+		$detector{"mother"}      = "targetCell";
+		$detector{"description"} = "Pb target";
+		$detector{"color"}       = "004488";
+		$detector{"type"}        = "Tube";
+		$detector{"pos"}         = "0 0 $zpos*mm";
 		$detector{"dimensions"}  = "0*mm $Rout*mm $length*mm 0*deg 360*deg";
 		$detector{"material"}    = "G4_Pb";
 		$detector{"style"}       = "1";
 		print_det(\%configuration, \%detector);
 
 
-		# Downstream Foil 50 microns Aluminum at 24.8 cm
-		my $ZCenter = 248 ;  # aluminum foil
+		# Upstream Foil 50 microns Aluminum at 24.8 cm
+		my $ZCenter = 0.1 - $Celllength ;  # upstream end
 		$Rout       = 5;  #
-		$length     = 0.0025; # (0.050 mm thick)
+		$length     = 0.015; # (0.030 mm thick)
 		%detector = init_det();
-		$detector{"name"}        = "AlTargetFoil";
-		$detector{"mother"}      = "root";
-		$detector{"description"} = "Aluminum Target Foil during Test ";
+		$detector{"name"}        = "AlTargetFoilUpstream";
+		$detector{"mother"}      = "targetCell";
+		$detector{"description"} = "Aluminum Upstream Foil ";
 		$detector{"pos"}         = "0 0 $ZCenter*mm";
-		$detector{"color"}       = "aa0000";
+		$detector{"color"}       = "aaaaaa";
 		$detector{"type"}        = "Tube";
 		$detector{"dimensions"}  = "0*mm $Rout*mm $length*mm 0*deg 360*deg";
 		$detector{"material"}    = "G4_Al";
 		$detector{"style"}       = "1";
 		print_det(\%configuration, \%detector);
+
+		# Downstream Foil 50 microns Aluminum at 24.8 cm
+		$ZCenter = $Celllength - 0.1 ;  # Downstream end
+		$Rout       = 5;  #
+		$length     = 0.015; # (0.030 mm thick)
+		%detector = init_det();
+		$detector{"name"}        = "AlTargetFoilDownstream";
+		$detector{"mother"}      = "targetCell";
+		$detector{"description"} = "Aluminum Downstream Foil ";
+		$detector{"pos"}         = "0 0 $ZCenter*mm";
+		$detector{"color"}       = "aaaaaa";
+		$detector{"type"}        = "Tube";
+		$detector{"dimensions"}  = "0*mm $Rout*mm $length*mm 0*deg 360*deg";
+		$detector{"material"}    = "G4_Al";
+		$detector{"style"}       = "1";
+		print_det(\%configuration, \%detector);
+
+		# flux detector downstream of the scattering chamber
+		$ZCenter = 300 ;  
+		$Rout       = 45;  #
+		$length     = 0.015; # (0.030 mm thick)
+		%detector = init_det();
+		$detector{"name"}        = "testFlux";
+		$detector{"mother"}      = "root";
+		$detector{"description"} = "Flux detector";
+		$detector{"pos"}         = "0 0 $ZCenter*mm";
+		$detector{"color"}       = "009900";
+		$detector{"type"}        = "Tube";
+		$detector{"dimensions"}  = "0*mm $Rout*mm $length*mm 0*deg 360*deg";
+		$detector{"material"}    = "G4_AIR";
+		$detector{"style"}       = "1";
+		$detector{"sensitivity"} = "flux";
+		$detector{"hit_type"}    = "flux";
+		$detector{"identifiers"} = "id manual 1";
+		print_det(\%configuration, \%detector);
+
 
 	} elsif($thisVariation eq "oldlH2" || $thisVariation eq "oldlD2") {
 		# vacuum container

464b8e3ee6c003c96500e600ba7438421349de25
Sangbaek <sangbaek@mit.edu>
2020-07-30 00:32:03 -0400

    replace the approximate he3 part with the stl file

--- clas12/targets/geometry.pl
+++ clas12/targets/geometry.pl
@@ -676,19 +676,6 @@ sub build_targets
 		print_det(\%configuration, \%detector);
 
 		%detector = init_det();
-		$detector{"name"}        = "he3target";
-		$detector{"mother"}      = "ltarget";
-		$detector{"description"} = "5 mm radius he-3 target";
-		$detector{"color"}       = "aa0000";
-		$detector{"type"}        = "Tube";
-		$detector{"pos"}         = "0*mm 0*mm 73.585*mm";
-		$detector{"dimensions"}  = "0*mm 5*mm 97.785*mm 0*deg 360*deg";
-		$detector{"material"}    = "polarizedHe3";
-		$detector{"style"}       = "1";
-		print_det(\%configuration, \%detector);
-
-
-		%detector = init_det();
 		$detector{"name"}        = "al_window_entrance";
 		$detector{"mother"}      = "ltarget";
 		$detector{"description"} = "5 mm radius aluminum window upstream";

976436efd104398054a86b5749344a71527f407a
maureeungaro <maureeungaro@gmail.com>
2020-12-08 15:04:43 -0500

    htcc hipo out

--- clas12/htcc/bank.pl
+++ clas12/htcc/bank.pl
@@ -31,9 +31,9 @@ sub define_bank
 	insert_bank_variable(\%configuration, $bankname, "ADC_order",    4, "Di", "always 0");
 	insert_bank_variable(\%configuration, $bankname, "ADC_ADC",      5, "Di", "ADC integral from pulse fit");
 	insert_bank_variable(\%configuration, $bankname, "ADC_time" ,    6, "Dd", "time from pulse fit");
-	insert_bank_variable(\%configuration, $bankname, "ADC_ped" ,     6, "Di", "pedestal from pulse analysis");
-	insert_bank_variable(\%configuration, $bankname, "TDC_order",    7, "Di", "always 0");
-	insert_bank_variable(\%configuration, $bankname, "TDC_TDC",      8, "Di", "TDC value");
+	insert_bank_variable(\%configuration, $bankname, "ADC_ped" ,     7, "Di", "pedestal from pulse analysis");
+	insert_bank_variable(\%configuration, $bankname, "TDC_order",    8, "Di", "always 0");
+	insert_bank_variable(\%configuration, $bankname, "TDC_TDC",      9, "Di", "TDC value");
 	insert_bank_variable(\%configuration, $bankname, "hitn",        99, "Di", "hit number");
 
 

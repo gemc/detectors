ae6e42ac69c973d325880f0c28c6a65907535eab
maureeungaro <maureeungaro@gmail.com>
2021-01-19 09:08:45 -0500

    indexing fix

--- clas12/ctof/bank.pl
+++ clas12/ctof/bank.pl
@@ -29,8 +29,8 @@ sub define_bank
 	insert_bank_variable(\%configuration, $bankname, "ADC_order",    4, "Di", "side of PMT");
 	insert_bank_variable(\%configuration, $bankname, "ADC_ADC",      5, "Di", "ADC value");
 	insert_bank_variable(\%configuration, $bankname, "ADC_time" ,    6, "Dd", "tdc*24.0/1000");
-	insert_bank_variable(\%configuration, $bankname, "ADC_ped" ,     6, "Di", "pedestal");
-	insert_bank_variable(\%configuration, $bankname, "TDC_order",    7, "Di", "side of PMT + 2");
-	insert_bank_variable(\%configuration, $bankname, "TDC_TDC",      8, "Di", "TDC value");
+	insert_bank_variable(\%configuration, $bankname, "ADC_ped" ,     7, "Di", "pedestal");
+	insert_bank_variable(\%configuration, $bankname, "TDC_order",    8, "Di", "side of PMT + 2");
+	insert_bank_variable(\%configuration, $bankname, "TDC_TDC",      9, "Di", "TDC value");
 	insert_bank_variable(\%configuration, $bankname, "hitn",        99, "Di", "hit number");
 }

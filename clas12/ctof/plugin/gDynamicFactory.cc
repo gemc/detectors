f1401a4b0f3e8a5ffb2b31450bf4ad45c6250f2f
maureeungaro <maureeungaro@gmail.com>
2021-08-17 18:33:46 -0400

    ported to sci-g

--- clas12/ctof/plugin/gDynamicFactory.cc
+++
@@ -1,7 +0,0 @@
-#include "ctof.h"
-
-// tells the DLL how to create a GDynamicFactory
-extern "C" GDynamic* GDynamicFactory(void) {
-	return static_cast<GDynamic*>(new ctofPlugin);
-}
-
--- clas12/ctof/plugin/loadSensitivePars.cc
+++
@@ -1,8 +0,0 @@
-#include "ctof.h"
-
-void ctofPlugin::loadSensitivePars(int runno, string variation)
-{
-	gSensitiveParameters = new GSensitivePars(10, 20);
-	
-	gSensitiveParameters->setHitBitset("000001");
-}

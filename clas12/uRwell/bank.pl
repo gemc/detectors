8790cced7b0637ed2444cdddb2af903c34c33354
mariangela-bondi <mariangela.bondi@ct.infn.it>
2022-03-10 12:36:34 +0100

    updated bank structure to hipo format

--- clas12/uRwell/bank.pl
+++ clas12/uRwell/bank.pl
@@ -15,20 +15,21 @@ our %configuration;
 # i for integers
 # d for doubles
 
-my $bankId   = 1300;
+my $bankId   = 2300;
 my $bankname = "uRwell";
 
 sub define_bank
 {
 	# uploading the hit definition
-	insert_bank_variable(\%configuration, $bankname, "bankid", $bankId, "Di", "$bankname bank ID");
-
-	insert_bank_variable(\%configuration, $bankname, "sector",       1, "Di", "sector (10..63)");
-	insert_bank_variable(\%configuration, $bankname, "layer",    3, "Di", "strip_layer number (1..2)");
-    insert_bank_variable(\%configuration, $bankname, "component",    4, "Di", "strip_ID number (1..10000)");
-    insert_bank_variable(\%configuration, $bankname, "ADC",    5, "Dd", "charge value");
-	insert_bank_variable(\%configuration, $bankname, "time",    6, "Dd", "time value");
-	insert_bank_variable(\%configuration, $bankname, "hitn",        99, "Di", "hit number");
+    insert_bank_variable(\%configuration, $bankname, "bankid",   $bankID, "Di", "$bankname bank ID");
+    insert_bank_variable(\%configuration, $bankname, "sector",       1, "Di", "sector number");
+    insert_bank_variable(\%configuration, $bankname, "layer",        2, "Di", "layer number");
+    insert_bank_variable(\%configuration, $bankname, "component",    3, "Di", "strip number");
+    insert_bank_variable(\%configuration, $bankname, "ADC_order",    4, "Di", "always 0");
+    insert_bank_variable(\%configuration, $bankname, "ADC_ADC",      5, "Di", "ADC");
+    insert_bank_variable(\%configuration, $bankname, "ADC_time" ,    6, "Dd", "always 0");
+    insert_bank_variable(\%configuration, $bankname, "ADC_ped" ,     7, "Di", "always 0");
+    insert_bank_variable(\%configuration, $bankname, "hitn",        99, "Di", "hit number");
 }
 
 

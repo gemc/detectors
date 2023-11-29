2468fc2ec233657b89e45c226ac6e771f736ff3d
mungaro <maurizio.ungaro@cnu.edu>
2023-11-29 15:58:22 -0500

    better install_coatjava.sh, fixed path to run-groovy

--- clas12/install_coatjava.sh
+++ clas12/install_coatjava.sh
@@ -1,81 +1,70 @@
 #!/bin/zsh
 
-# Linux: -c = do not get file if already done.
-if [[ $(uname) == "Linux" ]]; then
-  mwget="wget -c -nv --no-check-certificate"
-# Mac: makes it quiet and shows nice progress
-elif [[ $(uname) == "Darwin" ]]; then
-  mwget="wget -qc --show-progress --no-check-certificate"
-fi
+function mwget() {
+
+	# Linux: -c = do not get file if already done.
+	if [[ $(uname) == "Linux" ]]; then
+		wget -c -nv --no-check-certificate $@
+	# Mac: makes it quiet and shows nice progress
+	elif [[ $(uname) == "Darwin" ]]; then
+		wget -qc --show-progress --no-check-certificate $@
+	fi
+
+}
 
 rm -rf coat*jar jcsg*jar vecmath*jar
-
 
 # development. Set to no to use coatjava distribution instead
 USEDEVEL="no"
 githubRepo="https://github.com/JeffersonLab/coatjava"
-COATJAVA_TAG="10.0.2" # coatjava distribution - overwritten if USEDEVEL="yes"
 
 # if the argument '-d' is given, set USEDEVEL="yes"
-if [[ $1 == "-d" ]]; then
-  USEDEVEL="yes"
-fi
-
-# if the -t option is given, set the coatjava tag accordingly
-if [[ $1 == "-t" ]]; then
-  COATJAVA_TAG=$2
-fi
-
 # if the -g option is given, set the github url accordingly
-if [[ $1 == "-g" ]]; then
-  githubRepo=$2
-  USEDEVEL="custom"
+# if the -t option is given, set the coatjava tag accordingly
+if [[ $1 == "-d" ]]; then
+	USEDEVEL="yes"
+elif [[ $1 == "-g" ]]; then
+	githubRepo=$2
+	USEDEVEL="yes"
+elif [[ $1 == "-t" ]]; then
+	COATJAVA_TAG=$2
+	USEDEVEL="no"
+	tag_gz="https://github.com/JeffersonLab/coatjava/archive/refs/tags/$COATJAVA_TAG"".tar.gz"
 fi
-
-tag_gz="https://github.com/JeffersonLab/coatjava/releases/tag/$COATJAVA_TAGtar.gz"
 
 # print help if -h is given
 if [[ $1 == "-h" ]]; then
-  echo "Usage: update.sh [-d] [-t tag]"
-  echo "  -d: use coatjava development version"
-  echo "  -t tag: use coatjava tag version"
-  echo "  -g github_url: use a custom github url"
-  exit 0
+	echo "Usage: update.sh [-d] [-t tag]"
+	echo "  -d: use coatjava development version"
+	echo "  -t tag: use coatjava tag version"
+	echo "  -g github_url: use a custom github url"
+	exit 0
 fi
 
-rm -rf coatjava
+install_dir=coatjava
+src_dir=coatjava_src
+
+rm -rf $install_dir $src_dir
 
 if [[ $USEDEVEL == "yes" ]]; then
-
-  echo
-  echo "Cloning $githubRepo and building coatjava"
-  echo
-  git clone $githubRepo
-  cd coatjava
-  ./build-coatjava.sh
-  cp coatjava/lib/clas/* ..
-
-elif [[ $USEDEVEL == "custom" ]]; then
-  echo
-  echo "Cloning user defined $githubRepo and building coatjava"
-  echo
-  git clone $githubRepo coatjava
-  cd coatjava
-  ./build-coatjava.sh
-  cp coatjava/lib/clas/* ..
-
+	echo
+	echo "Cloning $githubRepo and building coatjava"
+	echo
+	git clone $githubRepo $src_dir
 else
+	# extract filename from $tag_gz
+	coatJavaFileName=$(echo $tag_gz | sed 's/.*\///')
+	echo "Downloading coatjava version $tag_gz onto $coatJavaFileName"
+	mwget --trust-server-names $tag_gz -O $coatJavaFileName
 
-  echo "Downloading coatjava version $tag_gz "
+	echo "Unpacking...$coatJavaFileName"
+	tar -xf $coatJavaFileName
+	rm -f $coatJavaFileName
 
-  # extract filename from $tag_gz
-  coatJavaFileName=$(echo $tag_gz | sed 's/.*\///')
-
-  $mwget --trust-server-names $tag_gz -O $coatJavaFileName
-
-  echo "Unpacking...$coatJavaFileName"
-
-  tar -xf $coatJavaFileName
-
-  cp coatjava/lib/clas/* .
+	mv coatjava-$COATJAVA_TAG $src_dir
 fi
+
+cd $src_dir
+./build-coatjava.sh
+cp coatjava/lib/clas/* ..
+cp -r coatjava ../$install_dir

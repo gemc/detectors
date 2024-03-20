d801b017201f9739291cf6da65317880f516fed8
Eric Fuchey <efuchey@jlab.org>
2024-03-20 00:35:19 -0400

    Removed the production of all files not related to alert target in targets.pl

--- clas12/alert/ALERT_target/targets.pl
+++ clas12/alert/ALERT_target/targets.pl
@@ -38,7 +38,8 @@ require "./geometry.pl";
 
 
 # all the scripts must be run for every configuration
-my @allConfs = ("lH2", "lD2", "ND3", "PolTarg", "12C", "63Cu", "118Sn", "208Pb", "27Al", "lH2e", "bonus", "alert");
+#my @allConfs = ("lH2", "lD2", "ND3", "PolTarg", "12C", "63Cu", "118Sn", "208Pb", "27Al", "lH2e", "bonus", "alert");
+my @allConfs = ("alert");
 
 foreach my $conf ( @allConfs )
 {

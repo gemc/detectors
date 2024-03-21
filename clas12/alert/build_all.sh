93029d6e140924d0ce9b96a8b050255c933f085c
Eric Fuchey <efuchey@jlab.org>
2024-03-21 03:16:06 -0400

    Added the factory groovy for atof. Also added the commands to build the geometries for atof and ahdc in build_all.sh

--- clas12/alert/build_all.sh
+++ clas12/alert/build_all.sh
@@ -1,7 +1,11 @@
 #commands to generate text files for alert.gcard
 cd ahdc
+run-groovy factory.groovy --variation default --runnumber 11
+run-groovy factory.groovy --variation rga_fall2018 --runnumber 11
 ./ahdc.pl config.dat
 cd ../atof
+run-groovy factory.groovy --variation default --runnumber 11
+run-groovy factory.groovy --variation rga_fall2018 --runnumber 11
 ./atof.pl config.dat
 cd ../ALERT_target
 ./targets.pl config.dat
@@ -9,3 +13,4 @@ cd ../external_shell_nonActif
 ./alertshell.pl config.dat
 cd ../He_bag
 ./hebag.pl config.dat
+cd ..

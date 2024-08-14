7d97de6e5587736b4108859131453784566b97e4
antorad <66285305+antorad@users.noreply.github.com>
2024-08-14 17:54:52 -0400

    added rge
    Added rge cad files in rge-dt directory, modified materials.pl and target.pl to include rge necessary geometries.

--- clas12/targets/geometry.pl
+++ clas12/targets/geometry.pl
@@ -3082,6 +3082,142 @@ sub build_targets
 	print_det(\%configuration, \%detector);
     }
 
+if($thisVariation eq "2cm-lD2-full")
+	{
+		my $nplanes = 4;
+
+		my @oradius  =  (    55,   55,  21.1,  21.1 );
+		# NOMINAL : (  -140.0,  265.0, 280.0, 280.0 );
+		my @z_plane  =  (  -135.0,  265.0, 280.0, 280.0 );
+
+		# vacuum target container
+		my %detector = init_det();
+		$detector{"name"}        = "target";
+		$detector{"mother"}      = "root";
+		$detector{"description"} = "Target Container";
+		$detector{"color"}       = "22ff22";
+		$detector{"type"}        = "Polycone";
+		my $dimen = "0.0*deg 360*deg $nplanes*counts";
+		for(my $i = 0; $i <$nplanes; $i++) {$dimen = $dimen ." 0.0*mm";}
+		for(my $i = 0; $i <$nplanes; $i++) {$dimen = $dimen ." $oradius[$i]*mm";}
+		for(my $i = 0; $i <$nplanes; $i++) {$dimen = $dimen ." $z_plane[$i]*mm";}
+		$detector{"dimensions"}  = $dimen;
+		$detector{"material"}    = "G4_Galactic";
+		$detector{"style"}       = 0;
+		print_det(\%configuration, \%detector);
+
+
+		$nplanes = 5;
+		my @oradiusT  =  (   2.5,  8.44,  7.3, 5.0,  2.5);
+		my @z_planeT  =  ( -4.2, -1.2, 12.5, 13.5, 14.5);
+
+		# actual lD2 target
+		%detector = init_det();
+		$detector{"name"}        = "lD2";
+		$detector{"mother"}      = "target";
+		$detector{"description"} = "Target Cell";
+		$detector{"color"}       = "aa0000";
+		$detector{"type"}        = "Polycone";
+		$dimen = "0.0*deg 360*deg $nplanes*counts";
+		for(my $i = 0; $i <$nplanes; $i++) {$dimen = $dimen ." 0.0*mm";}
+		for(my $i = 0; $i <$nplanes; $i++) {$dimen = $dimen ." $oradiusT[$i]*mm";}
+		for(my $i = 0; $i <$nplanes; $i++) {$dimen = $dimen ." $z_planeT[$i]*mm";}
+		$detector{"dimensions"}  = $dimen;
+		$detector{"material"}    = "LD2"; # defined in gemc database
+		$detector{"style"}       = 1;
+		print_det(\%configuration, \%detector);
+
+		# reference foil
+		my $thickness  = 0.01/2.;
+		my $zpos       = 35;
+		my $radius     = 7.3;
+		$detector{"name"}        = "refFoil";
+		$detector{"mother"}      = "target";
+		$detector{"description"} = "aluminum refernence foil";
+		$detector{"color"}       = "848789";
+		$detector{"type"}        = "Tube";
+		$detector{"pos"}         = "0 0 $zpos*mm";
+		$detector{"dimensions"}  = "0*mm $radius*mm $thickness*mm 0*deg 360*deg";
+		$detector{"material"}    = "G4_Al";
+		$detector{"style"}       = "1";
+		print_det(\%configuration, \%detector);
+
+		# bst tungsten shield
+		my $shradius 	= 104/2;
+		my $shthickness = 0.051/2.;
+		my $outradius 	= $shradius + $shthickness;
+		my $lenght 		= 180;
+		my $shzpos      = 50;
+		$detector{"name"}        = "Wshield";
+		$detector{"mother"}      = "target";
+		$detector{"description"} = "bst tungsten shield";
+		$detector{"color"}       = "606564";
+		$detector{"type"}        = "Tube";
+		$detector{"pos"}         = "0 0 $shzpos*mm";
+		$detector{"dimensions"}  = "$shradius*mm $outradius*mm $lenght*mm 0*deg 360*deg";
+		$detector{"material"}    = "G4_W";
+		$detector{"style"}       = "1";
+		print_det(\%configuration, \%detector);
+
+	}
+
+		if($thisVariation eq "2cm-lD2-empty")
+	{
+		my $nplanes = 4;
+
+		my @oradius  =  (    55,   55,  21.1,  21.1 );
+		# NOMINAL : (  -140.0,  265.0, 280.0, 280.0 );
+		my @z_plane  =  (  -125.0,  265.0, 280.0, 280.0 );
+
+		# vacuum target container
+		my %detector = init_det();
+		$detector{"name"}        = "target";
+		$detector{"mother"}      = "root";
+		$detector{"description"} = "Target Container";
+		$detector{"color"}       = "22ff22";
+		$detector{"type"}        = "Polycone";
+		my $dimen = "0.0*deg 360*deg $nplanes*counts";
+		for(my $i = 0; $i <$nplanes; $i++) {$dimen = $dimen ." 0.0*mm";}
+		for(my $i = 0; $i <$nplanes; $i++) {$dimen = $dimen ." $oradius[$i]*mm";}
+		for(my $i = 0; $i <$nplanes; $i++) {$dimen = $dimen ." $z_plane[$i]*mm";}
+		$detector{"dimensions"}  = $dimen;
+		$detector{"material"}    = "G4_Galactic";
+		$detector{"style"}       = 0;
+		print_det(\%configuration, \%detector);
+
+		# reference foil
+		my $thickness  = 0.01/2.;
+		my $zpos       = 35;
+		my $radius     = 7.3;
+		$detector{"name"}        = "refFoil";
+		$detector{"mother"}      = "target";
+		$detector{"description"} = "aluminum refernence foil";
+		$detector{"color"}       = "848789";
+		$detector{"type"}        = "Tube";
+		$detector{"pos"}         = "0 0 $zpos*mm";
+		$detector{"dimensions"}  = "0*mm $radius*mm $thickness*mm 0*deg 360*deg";
+		$detector{"material"}    = "G4_Al";
+		$detector{"style"}       = "1";
+		print_det(\%configuration, \%detector);
+
+		# bst tungsten shield
+		my $shradius 	= 104/2;
+		my $shthickness = 0.051/2.;
+		my $outradius 	= $shradius + $shthickness;
+		my $lenght 		= 180;
+		my $shzpos      = 50;
+		$detector{"name"}        = "Wshield";
+		$detector{"mother"}      = "target";
+		$detector{"description"} = "bst tungsten shield";
+		$detector{"color"}       = "606564";
+		$detector{"type"}        = "Tube";
+		$detector{"pos"}         = "0 0 $shzpos*mm";
+		$detector{"dimensions"}  = "$shradius*mm $outradius*mm $lenght*mm 0*deg 360*deg";
+		$detector{"material"}    = "G4_W";
+		$detector{"style"}       = "1";
+		print_det(\%configuration, \%detector);
+
+	}
 
 
 }
--- clas12/targets/materials.pl
+++ clas12/targets/materials.pl
@@ -656,4 +656,70 @@ sub materials
         print_mat(\%configuration, \%mat);
     }
 
+	if($thisVariation eq "2cm-lD2-full" || $thisVariation eq "2cm-lD2-empty")
+	{
+		# epoxy
+		%mat = init_mat();
+		$mat{"name"}          = "epoxy";
+		$mat{"description"}   = "epoxy glue 1.16 g/cm3";
+		$mat{"density"}       = "1.16";
+		$mat{"ncomponents"}   = "4";
+		$mat{"components"}    = "H 32 N 2 O 4 C 15";
+		print_mat(\%configuration, \%mat);
+
+		# fiberglass
+		%mat = init_mat();
+		$mat{"name"}          = "fiberglass";
+		$mat{"description"}   = "fiberglass in fr4 g/cm3";
+		$mat{"density"}       = "2.61";
+		$mat{"ncomponents"}   = "4";
+		$mat{"components"}    = "G4_SILICON_DIOXIDE 0.57 G4_CALCIUM_OXIDE 0.21 G4_ALUMINUM_OXIDE 0.14 G4_BORON_OXIDE 0.08";
+		print_mat(\%configuration, \%mat);
+
+		# FR4
+		%mat = init_mat();
+		$mat{"name"}          = "FR4";
+		$mat{"description"}   = "FR4 for circuit boards g/cm3";
+		$mat{"density"}       = "1.80";
+		$mat{"ncomponents"}   = "2";
+		$mat{"components"}    = "fiberglass 0.60 epoxy 0.40";
+		print_mat(\%configuration, \%mat);
+
+		# band
+		%mat = init_mat();
+		$mat{"name"}          = "band";
+		$mat{"description"}   = "Band made of fiberglass and cu similar to circuit board but different proportions g/cm3";
+		$mat{"density"}       = "2.99";
+		$mat{"ncomponents"}   = "2";
+		$mat{"components"}    = "FR4 0.50 G4_Cu 0.50";
+		print_mat(\%configuration, \%mat);
+
+		# carbon fiber
+		%mat = init_mat();
+		$mat{"name"}          = "carbonFiber";
+		$mat{"description"}   = "carbon fiber material is epoxy and carbon - 1.75g/cm3";
+		$mat{"density"}       = "1.75";
+		$mat{"ncomponents"}   = "2";
+		$mat{"components"}    = "G4_C 0.745 epoxy 0.255";
+		print_mat(\%configuration, \%mat);
+
+		# torlon4435
+		%mat = init_mat();
+		$mat{"name"}          = "torlon4435";
+		$mat{"description"}   = "torlon4435 - black (my guess) g/cm3";
+		$mat{"density"}       = "1.59";
+		$mat{"ncomponents"}   = "5";
+		$mat{"components"}    = "H 4 N 2 O 3 C 9 Ar 1";
+		print_mat(\%configuration, \%mat);
+
+		# torlon4203
+		%mat = init_mat();
+		$mat{"name"}          = "torlon4203";
+		$mat{"description"}   = "torlon4203 - yellow (my guess) g/cm3";
+		$mat{"density"}       = "1.41";
+		$mat{"ncomponents"}   = "5";
+		$mat{"components"}    = "H 4 N 2 O 3 C 9 Ar 1";
+		print_mat(\%configuration, \%mat);
+	}
+
 }

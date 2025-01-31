0ab720ed55b67b0dd84d1c1be0f52479fb72c3da
Maurizio Ungaro <maureeungaro@gmail.com>
2025-01-31 15:28:31 -0500

    clas12 repo moved to clas12Tags

--- clas12/ltcc/utils/show_segment.h
+++
@@ -1,6 +0,0 @@
-#ifndef LTCC_UTILS_SHOW_SEGMENT_LOADED
-#define LTCC_UTILS_SHOW_SEGMENT_LOADED
-
-void show_segment(int s);
-
-#endif
--- clas12/ltcc/utils/vars.C
+++
@@ -1,9 +0,0 @@
-#include "vars.h"
-
-// variable global indices
-// TODO: factor these into function arguments
-int PART = 0;
-int MIRROR = 0;
-int GAS = 0;
-int PMT = 0;
-int WC = 0;
